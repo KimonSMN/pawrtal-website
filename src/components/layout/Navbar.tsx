e561e84b4164c332e87de4298e5cf7add992ba17
KimonSMN <kimonsmirlianos@gmail.com>
2025-12-03 22:56:41 +0200

    Navbar Created

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import logo from "../../assets/pawrtal_logo.png";
+import profile from "../../assets/profile.png";
 
 function Navbar() {
     return (
-        <>
-            <div> Navbar </div>
-        </>
+        <header className="w-full py-3 border-b-2 border-gray-300 bg-gray-100 fixed top-0 left-0">
+            <div className="px-4 flex justify-between items-center">
+                {/* Logo & Pawrtal text */}
+                <div className="flex items-center gap-3 hover:cursor-pointer">
+                    <img className="h-7" src={logo} alt="Pawrtal logo" />
+                    <div className="text-xl font-bold">Pawrtal</div>
+                </div>
+
+                {/* Navigation */}
+                <nav className="flex items-center space-x-6">
+                    <a href="#information" className="hover:text-gray-500">
+                        Πληροφορίες
+                    </a>
+                    <a href="#contact" className="hover:text-gray-500">
+                        Επικοινωνία
+                    </a>
+                    <img className="h-6 w-6 object-contain" src={profile} alt="Profile icon" />
+                </nav>
+            </div>
+        </header>
     );
 }
 
