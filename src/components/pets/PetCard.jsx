e6ad1af47c6e2ec9a5f306fa26660ca9b951ad1e
KimonSMN <kimonsmirlianos@gmail.com>
2025-12-04 17:32:27 +0200

    hover effect on petcard

--- src/components/pets/PetCard.jsx
+++ src/components/pets/PetCard.jsx
@@ -3,7 +3,14 @@ import dog_image from "../../assets/dog_1.jpg";
 
 function PetCard() {
     return (
-        <div className="w-full bg-white rounded-2xl overflow-hidden shadow-lg border border-gray-200 flex flex-col">
+        <div
+            className="
+                w-full bg-white rounded-2xl overflow-hidden shadow-lg border border-gray-200 
+                flex flex-col cursor-pointer 
+                transition-transform duration-300 ease-out 
+                hover:scale-105
+            "
+        >
             {/* Image section */}
             <div className="h-60 w-full">
                 <img src={dog_image} alt="Pet" className="w-full h-full object-cover" />
